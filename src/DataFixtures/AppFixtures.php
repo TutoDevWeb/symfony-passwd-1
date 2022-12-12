4dd75d4a7e66233ef16deb1d48432fdb9fc1d94d
TutoDevWeb <TutoDevWeb@outlook.fr>
2022-12-12 15:18:02 +0100

    Finalement j'installe les fixtures standard.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Passwd;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,16 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        for ($i = 0; $i < 10; $i++) {
+            $passwd = new Passwd();
+            $passwd->setWebSite("Site Web $i");
+            $passwd->setLogin("Login $i");
+            $passwd->setPassword("Password $i");
+            $passwd->setCreatedAt(new \DateTimeImmutable);
+            $passwd->setUpdatedAt(new \DateTimeImmutable);
+            $manager->persist($passwd);
+        }
 
         $manager->flush();
     }
